70c0966fe2630f41d1c3f8185783f9ac3b192288
iameru <post@i3o.eu>
2023-02-02 11:56:54 +0100

    bcrypt round testing

---
+++ bcryptRounds.js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcrypt");
+
+pw = "Our AverageE Joe'Zz Pasword123!";
+
+function timeBcryptHash(round) {
+  const start = new Date();
+  bcrypt.hashSync(pw, round);
+  const stop = new Date();
+
+  return stop - start;
+}
+
+//-- maxtime in miliseconds
+function main(maxtime = 1234) {
+  console.log(
+    `trying for bcryptcosts with maxtime: ${maxtime / 1000} seconds to hash`
+  );
+
+  const rounds = Array.from({ length: 20 }, (_, i) => i + 4);
+
+  rounds.map((round) => {
+    const timedResult = timeBcryptHash(round);
+
+    // found the sweetspot?
+    if (timedResult > maxtime) {
+      const retryRound = round - 1;
+
+      console.log("rechecking last valid round");
+
+      console.log("-------------- Result -----------------");
+      for (i = 0; i < 3; i++) {
+        const timedResult = timeBcryptHash(retryRound);
+        console.log(
+          `hashed ${retryRound} rounds in ${timedResult} miliseconds`
+        );
+      }
+      console.log(
+        `${retryRound} rounds mean ${
+          2 ** retryRound
+        } iterations / bcrypt factor`
+      );
+      console.log("");
+
+      process.exit(0);
+    }
+    console.log(`hashed ${round} rounds in ${timedResult} miliseconds`);
+  });
+}
+
+main();
